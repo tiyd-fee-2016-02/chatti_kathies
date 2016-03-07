3377a27c7651c3f120e368d6f4433b1f513e73e9
Dennis Goldin <dennis.goldin@gmail.com>
2016-03-07 18:20:19 -0500

    API call testing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,25 @@
-// app.js
+$(function () {
+  'use strict';
+
+console.log(getJokes());
+
+  new Vue({
+    el: '.chatBox',
+    data: {
+      newChatMsg: '',
+      inputMsgs: [
+        // { text: 'Add some text' }
+      ]
+    },
+    methods: {
+      processMsg: function () {
+        var text = this.newChatMsg.trim()
+        if (text) {
+          this.inputMsgs.push({ text: text })
+          this.newChatMsg = ''
+        }
+      }
+    }
+  })
+
+});  // End of file.
---
+++ assets/js/chat_functions/jokes.js
@@ -0,0 +1,46 @@
+
+
+function getJokes() {
+
+  var str = "Yes!!!";
+
+// Working API URLs
+  // var myUrl = "http://api.github.com/users/octocat";
+  // var myUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+  //               "id=4464368" +    // Durham, NC, US
+  //               "&cnt=1" +        // 1 record from 5 Day, 3 Hour Forecast
+  //               "&APPID=82f61d5df7730f4b96d58ed8e8aa6b63";
+
+// Broken API URLs
+var myUrl = "http://tambal.azurewebsites.net/joke/random";
+// var myUrl = "https://getpuns.herokuapp.com/api/random";
+
+$.ajax({
+    beforeSend: function(request) {
+        // request.setRequestHeader("Access-Control-Allow-Origin", "*");
+        request.setRequestHeader("Accept", "application/json");
+    },
+    crossDomain: true,
+    dataType: "json",
+    url: myUrl,
+    success: function(data) {
+        //Your code
+    console.log(data);
+    }
+});
+
+  // $.getJSON((url), function (data) {
+
+    // console.log(data.joke);
+    // console.log(data);
+
+  // });   end JSON call
+
+  return str;
+
+}
+
+
+
+// curl --get --include 'http://tambal.azurewebsites.net/joke/random' \
+//   -H 'Accept: application/json'
